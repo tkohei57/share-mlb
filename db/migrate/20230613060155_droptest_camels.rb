75820cb0a22c5b528a8c082cfc8ee9d2e58cef41
tkohei57 <con.kt.57@gmail.com>
2023-06-13 23:05:14 +0900

    マイグレーションファイルの修正
    誤って作成してしまったtest_camelsテーブルに関する条件分岐を追加

--- db/migrate/20230613060155_droptest_camels.rb
+++ db/migrate/20230613060155_droptest_camels.rb
@@ -1,8 +1,10 @@
 class DroptestCamels < ActiveRecord::Migration[6.0]
   def change
-    drop_table :test_camels do |t|
-      t.references :article, null: false, foreign_key: true
-      t.references :tag, null: false, foreign_key: true
+    if table_exists?(:test_camels)
+      drop_table :test_camels do |t|
+        t.references :article, null: false, foreign_key: true
+        t.references :tag, null: false, foreign_key: true
+      end
     end
   end
 end
